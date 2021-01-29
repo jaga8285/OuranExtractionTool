e33ff7bb8647df8f41640b9777f2c72c6638b355
João Andrade <joao.ag.andrade@gmail.com>
2021-01-29 01:02:02 +0000

    Pointer reading now works on big files

--- extraction_script.py
+++ extraction_script.py
@@ -9,11 +9,7 @@ from io import BufferedReader
 
 CODEDICT={b"\x00\x3c":"Dialog",b"\x00\x3e":"Speaker",b"\x00\x39":"Choice",b"\x00\x46":"Chapter name",}
 def findPointers(filename):
-    #files = listdir()
     pointers = []
-    """ for file in files:
-        if (file[-4:] == ".bin"):
-            binfiles.append(file) """
     fbin = BufferedReader(open(filename,"rb")) #BufferedReader allows peeking ahead without moving the cursor
     fbin.read(4) #TODO check if 00000000
     textaddrbin = fbin.read(4)
@@ -30,12 +26,12 @@ def findPointers(filename):
     while fbin.tell() < textaddr:  #fbin.tell() returns cursor position. pretty neat, iterate pointer table region until you reach actual memory
         buffer = (fbin.read(1),fbin.read(1))
         peekresult = fbin.peek(8) # read 4 bytes ahead without moving the cursor
-        cursor = fbin.tell()            
+        #cursor = fbin.tell()            
         if (peekresult[0:1] == b'\x03' and peekresult[1:2] == b'\x01'):
             scripttype = buffer[1] + buffer[0] 
-            bytescount = peekresult[3:4] + peekresult[2:3] #indexing a byte directly converts it into an int. need to slice it like this.
-            offset = peekresult[7:8] + peekresult[6:7] + peekresult[5:6] + peekresult[4:5]
-            print("FOUND A POINTER in position {:X}! Type = {}  Size = {}  Offset = {}".format(fbin.tell(),scripttype.hex(),bytescount.hex(),offset.hex()))
+            bytescount = peekresult[3] * 256 + peekresult[2] #indexing a byte directly converts it into an int. need to slice it like this.
+            offset = (peekresult[7] *256 + peekresult[6]) * 0x7fff + peekresult[5] * 256 + peekresult[4]
+            #print("FOUND A POINTER in position {:X}! Type = {}  Size = {}  Offset = {}".format(fbin.tell(),scripttype.hex(),bytescount.hex(),offset.hex()))
             pointers.append((fbin.tell(), scripttype, bytescount, offset))
     fbin.close()
     return (pointers,textaddr)
@@ -44,28 +40,20 @@ def genText(pointers,filename,textaddr):
     ftxt = open(filename[:-4]+".txt","w", encoding="shiftjis")
     fbin = open(filename,"rb")
     for pointer in pointers: 
-        print(pointer)
         try:
             ftxt.write("Type: {}\n".format(CODEDICT[pointer[1]]))
         except KeyError:
             #ftxt.write("Type: {}\n".format(pointer[1]))  #these are unknown pointer types, enable them if you want
             continue
-        ftxt.write("Size: {}\n".format(int.from_bytes(pointer[2],"big")))  
-        print("Size: {}".format(int.from_bytes(pointer[2],"big")))  
-        ftxt.write("Offset: {}\n".format(pointer[3].hex()))
-
-        ftxt.write("Text Position: {:x}\n".format((int.from_bytes(pointer[3],"big")+textaddr)))
-        ftxt.write("Pointer Position: {}\n".format(pointer[0]))
-        print(int.from_bytes(pointer[3],"big")+textaddr)
-        print(fbin.tell())    
-        while fbin.tell() < int.from_bytes(pointer[3],"big")+textaddr:
+        ftxt.write("Size: {}\n".format(pointer[2]))  
+        ftxt.write("Offset: {:x}\n".format(pointer[3]))
+        ftxt.write("Text Position: {:x}\n".format(pointer[3]+textaddr))
+        ftxt.write("Pointer Position: {:x}\n".format(pointer[0]))   
+        while fbin.tell() < pointer[3]+textaddr:
             fbin.read(1)
-        print(fbin.tell())    
-        pointercontents = fbin.read(int.from_bytes(pointer[2],"big"))
-        #print(pointercontents)
+        pointercontents = fbin.read(pointer[2])
         ftxt.write(pointercontents.decode("shiftjis"))
         ftxt.write("\n\n")
-        break
     ftxt.close()
     fbin.close()
 
